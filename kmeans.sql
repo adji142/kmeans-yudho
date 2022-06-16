c0c2be729e384edd7c938b7af4a209847e167ffe
adji142 <prasetyoajiw@gmail.com>
2022-06-16 20:11:12 +0700

    lll

--- kmeans.sql
+++ kmeans.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : AISServer
  Source Server Type    : MySQL
- Source Server Version : 100210
+ Source Server Version : 100240
  Source Host           : localhost:3306
  Source Schema         : kmeans
 
  Target Server Type    : MySQL
- Target Server Version : 100210
+ Target Server Version : 100240
  File Encoding         : 65001
 
- Date: 06/04/2021 19:17:50
+ Date: 15/06/2022 16:56:09
 */
 
 SET NAMES utf8mb4;
@@ -89,9 +89,9 @@ CREATE TABLE `tcentroid`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `KodeData` int(255) NOT NULL,
   `Centroid` int(11) NOT NULL,
-  `LuasPanen` double(16, 4) NOT NULL,
-  `Produksi` double(16, 4) NOT NULL,
-  `xother` double(16, 4) NOT NULL,
+  `Pekerjaan` double(16, 4) NOT NULL,
+  `Penghasilan` double(16, 4) NOT NULL,
+  `Pendidikan` double(16, 4) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
@@ -99,14 +99,43 @@ CREATE TABLE `tcentroid`  (
 -- Records of tcentroid
 -- ----------------------------
 INSERT INTO `tcentroid` VALUES (1, 11, 1, 0.0000, 0.0000, 0.0000);
-INSERT INTO `tcentroid` VALUES (2, 15, 2, 0.7286, 0.7565, 0.0000);
-INSERT INTO `tcentroid` VALUES (3, 19, 3, 1.0000, 1.0000, 0.0000);
+INSERT INTO `tcentroid` VALUES (2, 15, 2, 0.3333, 0.7500, 1.0000);
+INSERT INTO `tcentroid` VALUES (3, 19, 3, 1.0000, 0.7500, 0.6666);
 
 -- ----------------------------
 -- Table structure for tdata
 -- ----------------------------
 DROP TABLE IF EXISTS `tdata`;
 CREATE TABLE `tdata`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nama` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `Alamat` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `Pekerjaan` double(16, 4) NOT NULL,
+  `Penghasilan` double(16, 4) NOT NULL,
+  `Pendidikan` double(16, 4) NULL DEFAULT NULL,
+  `Koordinat` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tdata
+-- ----------------------------
+INSERT INTO `tdata` VALUES (1, 'Wagiyati', 'Kecamatan Kalijambe, Sragen Regency, Central Java, Indonesia', 4.0000, 1820000.0000, 5.0000, '-7.4354026,110.8195292');
+INSERT INTO `tdata` VALUES (2, 'Lanjar Sutopo', 'Kecamatan Plupuh, Sragen Regency, Central Java, Indonesia', 3.0000, 1650000.0000, 3.0000, '-7.4319067,110.8910556');
+INSERT INTO `tdata` VALUES (3, 'Supardi', 'Kecamatan Masaran, Sragen Regency, Central Java, Indonesia', 3.0000, 1520000.0000, 3.0000, '-7.470387199999999,110.9387418');
+INSERT INTO `tdata` VALUES (4, 'Tukiman Citro', 'Kecamatan Kedawung, Sragen Regency, Central Java, Indonesia', 2.0000, 1000000.0000, 2.0000, '-7.4860782,111.0341183');
+INSERT INTO `tdata` VALUES (5, 'Sutanto', 'Kecamatan Sambirejo, Sragen Regency, Central Java, Indonesia', 4.0000, 1600000.0000, 4.0000, '-7.502921799999999,111.1056539');
+INSERT INTO `tdata` VALUES (6, 'Joko Susilo', 'Kecamatan Gondang, Sragen Regency, Central Java, Indonesia', 4.0000, 1300000.0000, 4.0000, '-7.4417327,111.1056539');
+INSERT INTO `tdata` VALUES (7, 'Kevin', 'Sambung Macan, Sragen Regency, Central Java, Indonesia', 5.0000, 2200000.0000, 5.0000, '-7.380601700000001,111.1056539');
+INSERT INTO `tdata` VALUES (8, 'Mulyono', 'Ngrampal, Sragen Regency, Central Java, Indonesia', 3.0000, 1550000.0000, 3.0000, '-7.3883305,111.052002');
+INSERT INTO `tdata` VALUES (9, 'Samidi', 'Karangmalang, Sragen Regency, Central Java, Indonesia', 2.0000, 1300000.0000, 3.0000, '-7.468032399999998,110.9864294');
+INSERT INTO `tdata` VALUES (10, 'Juri', 'Kecamatan Sragen, Sragen Regency, Central Java, Indonesia', 4.0000, 1750000.0000, 3.0000, '-7.430520199999999,111.0090622');
+
+-- ----------------------------
+-- Table structure for tdata_copy1_copy1
+-- ----------------------------
+DROP TABLE IF EXISTS `tdata_copy1_copy1`;
+CREATE TABLE `tdata_copy1_copy1`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Nama` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `Alamat` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
@@ -119,28 +148,28 @@ CREATE TABLE `tdata`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of tdata
+-- Records of tdata_copy1_copy1
 -- ----------------------------
-INSERT INTO `tdata` VALUES (1, 'Kalijambe', 'Kecamatan Kalijambe, Sragen Regency, Central Java, Indonesia', 4255.0000, 26536.0000, 0.0000, '-7.4354026,110.8195292', '2019');
-INSERT INTO `tdata` VALUES (2, 'Plupuh', 'Kecamatan Plupuh, Sragen Regency, Central Java, Indonesia', 6409.0000, 39830.0000, 0.0000, '-7.4319067,110.8910556', '2019');
-INSERT INTO `tdata` VALUES (3, 'Masaran', 'Kecamatan Masaran, Sragen Regency, Central Java, Indonesia', 7794.0000, 52697.0000, 0.0000, '-7.470387199999999,110.9387418', '2019');
-INSERT INTO `tdata` VALUES (4, 'Kedawung', 'Kecamatan Kedawung, Sragen Regency, Central Java, Indonesia', 5354.0000, 34261.0000, 0.0000, '-7.4860782,111.0341183', '2019');
-INSERT INTO `tdata` VALUES (5, 'Sambirejo', 'Kecamatan Sambirejo, Sragen Regency, Central Java, Indonesia', 3015.0000, 19278.0000, 0.0000, '-7.502921799999999,111.1056539', '2019');
-INSERT INTO `tdata` VALUES (6, 'Gondang', 'Kecamatan Gondang, Sragen Regency, Central Java, Indonesia', 7217.0000, 45500.0000, 0.0000, '-7.4417327,111.1056539', '2019');
-INSERT INTO `tdata` VALUES (7, 'Sambung Macan', 'Sambung Macan, Sragen Regency, Central Java, Indonesia', 6469.0000, 40885.0000, 0.0000, '-7.380601700000001,111.1056539', '2019');
-INSERT INTO `tdata` VALUES (8, 'Ngrampal', 'Ngrampal, Sragen Regency, Central Java, Indonesia', 6850.0000, 43338.0000, 0.0000, '-7.3883305,111.052002', '2019');
-INSERT INTO `tdata` VALUES (9, 'Karangmalang', 'Karangmalang, Sragen Regency, Central Java, Indonesia', 6894.0000, 43568.0000, 0.0000, '-7.468032399999998,110.9864294', '2019');
-INSERT INTO `tdata` VALUES (10, 'Sragen', 'Kecamatan Sragen, Sragen Regency, Central Java, Indonesia', 4340.0000, 29227.0000, 0.0000, '-7.430520199999999,111.0090622', '2019');
-INSERT INTO `tdata` VALUES (11, 'Sidoharjo', 'Sidoharjo, Sragen Regency, Central Java, Indonesia', 9161.0000, 61790.0000, 0.0000, '-7.4283992,110.9625854', '2019');
-INSERT INTO `tdata` VALUES (12, 'Tanon', 'Tanon, Sragen Regency, Central Java, Indonesia', 7064.0000, 43982.0000, 0.0000, '-7.389940899999999,110.9148985', '2019');
-INSERT INTO `tdata` VALUES (13, 'Gemolong', 'Gemolong, Sragen Regency, Central Java, Indonesia', 4224.0000, 26241.0000, 0.0000, '-7.393421899999999,110.8433709', '2019');
-INSERT INTO `tdata` VALUES (14, 'Miri', 'Kecamatan Miri, Sragen Regency, Central Java, Indonesia', 2485.0000, 15377.0000, 0.0000, '-7.348972299999999,110.8135688', '2019');
-INSERT INTO `tdata` VALUES (15, 'Sumberlawang', 'Sumberlawang, Sragen Regency, Central Java, Indonesia', 3724.0000, 22902.0000, 0.0000, '-7.3310958,110.8672131', '2019');
-INSERT INTO `tdata` VALUES (16, 'Mondokan', 'Mondokan, Sragen Regency, Central Java, Indonesia', 2374.0000, 14621.0000, 0.0000, '-7.307272599999998,110.9387418', '2019');
-INSERT INTO `tdata` VALUES (17, 'Sukodono', 'Sukodono, Sidoarjo Regency, East Java, Indonesia', 3780.0000, 23225.0000, 0.0000, '-7.394667,112.6737856', '2019');
-INSERT INTO `tdata` VALUES (18, 'Gesi', 'Gesi, Sragen Regency, Central Java, Indonesia', 1744.0000, 10754.0000, 0.0000, '-7.3241711,111.0102737', '2019');
-INSERT INTO `tdata` VALUES (19, 'Tangen', 'Tangen, Sragen Regency, Central Java, Indonesia', 1699.0000, 10386.0000, 0.0000, '-7.2811574,111.0579632', '2019');
-INSERT INTO `tdata` VALUES (20, 'Jenar', 'Jenar, Sragen Regency, Central Java, Indonesia', 1814.0000, 11311.0000, 0.0000, '-7.3195291,111.1056539', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (1, 'Kalijambe', 'Kecamatan Kalijambe, Sragen Regency, Central Java, Indonesia', 4255.0000, 26536.0000, 0.0000, '-7.4354026,110.8195292', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (2, 'Plupuh', 'Kecamatan Plupuh, Sragen Regency, Central Java, Indonesia', 6409.0000, 39830.0000, 0.0000, '-7.4319067,110.8910556', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (3, 'Masaran', 'Kecamatan Masaran, Sragen Regency, Central Java, Indonesia', 7794.0000, 52697.0000, 0.0000, '-7.470387199999999,110.9387418', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (4, 'Kedawung', 'Kecamatan Kedawung, Sragen Regency, Central Java, Indonesia', 5354.0000, 34261.0000, 0.0000, '-7.4860782,111.0341183', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (5, 'Sambirejo', 'Kecamatan Sambirejo, Sragen Regency, Central Java, Indonesia', 3015.0000, 19278.0000, 0.0000, '-7.502921799999999,111.1056539', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (6, 'Gondang', 'Kecamatan Gondang, Sragen Regency, Central Java, Indonesia', 7217.0000, 45500.0000, 0.0000, '-7.4417327,111.1056539', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (7, 'Sambung Macan', 'Sambung Macan, Sragen Regency, Central Java, Indonesia', 6469.0000, 40885.0000, 0.0000, '-7.380601700000001,111.1056539', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (8, 'Ngrampal', 'Ngrampal, Sragen Regency, Central Java, Indonesia', 6850.0000, 43338.0000, 0.0000, '-7.3883305,111.052002', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (9, 'Karangmalang', 'Karangmalang, Sragen Regency, Central Java, Indonesia', 6894.0000, 43568.0000, 0.0000, '-7.468032399999998,110.9864294', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (10, 'Sragen', 'Kecamatan Sragen, Sragen Regency, Central Java, Indonesia', 4340.0000, 29227.0000, 0.0000, '-7.430520199999999,111.0090622', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (11, 'Sidoharjo', 'Sidoharjo, Sragen Regency, Central Java, Indonesia', 9161.0000, 61790.0000, 0.0000, '-7.4283992,110.9625854', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (12, 'Tanon', 'Tanon, Sragen Regency, Central Java, Indonesia', 7064.0000, 43982.0000, 0.0000, '-7.389940899999999,110.9148985', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (13, 'Gemolong', 'Gemolong, Sragen Regency, Central Java, Indonesia', 4224.0000, 26241.0000, 0.0000, '-7.393421899999999,110.8433709', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (14, 'Miri', 'Kecamatan Miri, Sragen Regency, Central Java, Indonesia', 2485.0000, 15377.0000, 0.0000, '-7.348972299999999,110.8135688', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (15, 'Sumberlawang', 'Sumberlawang, Sragen Regency, Central Java, Indonesia', 3724.0000, 22902.0000, 0.0000, '-7.3310958,110.8672131', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (16, 'Mondokan', 'Mondokan, Sragen Regency, Central Java, Indonesia', 2374.0000, 14621.0000, 0.0000, '-7.307272599999998,110.9387418', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (17, 'Sukodono', 'Sukodono, Sidoarjo Regency, East Java, Indonesia', 3780.0000, 23225.0000, 0.0000, '-7.394667,112.6737856', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (18, 'Gesi', 'Gesi, Sragen Regency, Central Java, Indonesia', 1744.0000, 10754.0000, 0.0000, '-7.3241711,111.0102737', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (19, 'Tangen', 'Tangen, Sragen Regency, Central Java, Indonesia', 1699.0000, 10386.0000, 0.0000, '-7.2811574,111.0579632', '2019');
+INSERT INTO `tdata_copy1_copy1` VALUES (20, 'Jenar', 'Jenar, Sragen Regency, Central Java, Indonesia', 1814.0000, 11311.0000, 0.0000, '-7.3195291,111.1056539', '2019');
 
 -- ----------------------------
 -- Table structure for thasil
@@ -152,31 +181,21 @@ CREATE TABLE `thasil`  (
   `Keanggotaan` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `iterasi` int(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of thasil
 -- ----------------------------
-INSERT INTO `thasil` VALUES (1, 1, 'C2', 3);
-INSERT INTO `thasil` VALUES (2, 2, 'C1', 3);
-INSERT INTO `thasil` VALUES (3, 3, 'C1', 3);
-INSERT INTO `thasil` VALUES (4, 4, 'C2', 3);
-INSERT INTO `thasil` VALUES (5, 5, 'C3', 3);
-INSERT INTO `thasil` VALUES (6, 6, 'C1', 3);
+INSERT INTO `thasil` VALUES (1, 1, 'C1', 3);
+INSERT INTO `thasil` VALUES (2, 2, 'C3', 3);
+INSERT INTO `thasil` VALUES (3, 3, 'C3', 3);
+INSERT INTO `thasil` VALUES (4, 4, 'C3', 3);
+INSERT INTO `thasil` VALUES (5, 5, 'C2', 3);
+INSERT INTO `thasil` VALUES (6, 6, 'C2', 3);
 INSERT INTO `thasil` VALUES (7, 7, 'C1', 3);
-INSERT INTO `thasil` VALUES (8, 8, 'C1', 3);
-INSERT INTO `thasil` VALUES (9, 9, 'C1', 3);
+INSERT INTO `thasil` VALUES (8, 8, 'C3', 3);
+INSERT INTO `thasil` VALUES (9, 9, 'C3', 3);
 INSERT INTO `thasil` VALUES (10, 10, 'C2', 3);
-INSERT INTO `thasil` VALUES (11, 11, 'C1', 3);
-INSERT INTO `thasil` VALUES (12, 12, 'C1', 3);
-INSERT INTO `thasil` VALUES (13, 13, 'C2', 3);
-INSERT INTO `thasil` VALUES (14, 14, 'C3', 3);
-INSERT INTO `thasil` VALUES (15, 15, 'C2', 3);
-INSERT INTO `thasil` VALUES (16, 16, 'C3', 3);
-INSERT INTO `thasil` VALUES (17, 17, 'C2', 3);
-INSERT INTO `thasil` VALUES (18, 18, 'C3', 3);
-INSERT INTO `thasil` VALUES (19, 19, 'C3', 3);
-INSERT INTO `thasil` VALUES (20, 20, 'C3', 3);
 
 -- ----------------------------
 -- Table structure for userrole
@@ -228,9 +247,23 @@ INSERT INTO `users` VALUES (43, 'operator', 'Operator', 'a9bdd47d7321d4089b3b005
 DROP VIEW IF EXISTS `vw_normalisasidata`;
 CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `vw_normalisasidata` AS SELECT 
 	a.id, a.Nama,
-	((((a.LuasPanen-(SELECT MIN(luaspanen) FROM tdata))*(0-1))/((SELECT MAX(luaspanen) FROM tdata)-(SELECT MIN(luaspanen) FROM tdata)))+1) ND_LuasPanen,
-	((((a.Produksi-(SELECT MIN(Produksi) FROM tdata))*(0-1))/((SELECT MAX(Produksi) FROM tdata)-(SELECT MIN(Produksi) FROM tdata)))+1) ND_Produksi,
-	0 ND_Other
+	(a.Pekerjaan-(SELECT MAX(Pekerjaan) FROM tdata))/((SELECT MIN(Pekerjaan) FROM tdata) - (SELECT MAX(Pekerjaan) FROM tdata)) ND_Pekerjaan,
+	(a.Penghasilan-(SELECT MAX(Penghasilan) FROM tdata))/((SELECT MIN(Penghasilan) FROM tdata) - (SELECT MAX(Penghasilan) FROM tdata)) ND_Penghasilan,
+	(a.Pendidikan-(SELECT MAX(Pendidikan) FROM tdata))/((SELECT MIN(Pendidikan) FROM tdata) - (SELECT MAX(Pendidikan) FROM tdata)) ND_Pendidikan
 FROM tdata a ;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, 'Wagiyati', 0.33333333, 0.31666667, -0.00000000);
+INSERT INTO `users` VALUES (2, 'Lanjar Sutopo', 0.66666667, 0.45833333, 0.66666667);
+INSERT INTO `users` VALUES (3, 'Supardi', 0.66666667, 0.56666667, 0.66666667);
+INSERT INTO `users` VALUES (4, 'Tukiman Citro', 1.00000000, 1.00000000, 1.00000000);
+INSERT INTO `users` VALUES (5, 'Sutanto', 0.33333333, 0.50000000, 0.33333333);
+INSERT INTO `users` VALUES (6, 'Joko Susilo', 0.33333333, 0.75000000, 0.33333333);
+INSERT INTO `users` VALUES (7, 'Kevin', -0.00000000, -0.00000000, -0.00000000);
+INSERT INTO `users` VALUES (8, 'Mulyono', 0.66666667, 0.54166667, 0.66666667);
+INSERT INTO `users` VALUES (9, 'Samidi', 1.00000000, 0.75000000, 0.66666667);
+INSERT INTO `users` VALUES (10, 'Juri', 0.33333333, 0.37500000, 0.66666667);
 
 SET FOREIGN_KEY_CHECKS = 1;
